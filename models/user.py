8673aa750f774214ec49bc9122cadc67b77c811a
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-21 01:10:53 +0200

    user.py

--- models/user.py
+++ models/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
