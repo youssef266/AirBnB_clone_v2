e04af617a4c5420483e016362c90c63c77958a35
ayaelas <assalharras@gmail.com>
2024-02-20 20:07:59 +0000

    r

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""Defines the city class.""" 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Foreignkey
+from sqlalchemy.orm import relationship
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = column(String(60), Foreignkey("states.id"), nullable=False)
+        places = relationship("Place", backref = "cities", cascade = "delete")
