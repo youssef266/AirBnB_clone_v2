c9c82c3524480140a9ffd6107c370744cd7bc5e0
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-20 16:09:12 +0200

    alx

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    
+    def delete(self, obj=None):
+        """ delete an existing element
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
