5c4c4486f5ebe5f6bb0f991e2017bf1fe59d8fc8
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-22 00:18:23 +0200

    user.py

--- models/user.py
+++ models/user.py
@@ -8,24 +8,15 @@ from sqlalchemy.orm import relationship
 dbstorage = getenv('HBNB_TYPE_STORAGE')
 
 class User(BaseModel, Base):
-    __tablename__ = "users"
     """This is the class for user
     Attributes:
-        email: email address String(128) can't be null
-        password: password for you login String(128)
-        first_name: first name String(128)
-        last_name: last name String(128)
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    if dbstorage == 'database':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        
-        places = relationship("Place",  backref="user", cascade="all,delete")
-        reviews = relationship("Review",  backref="user", cascade="all,delete")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
