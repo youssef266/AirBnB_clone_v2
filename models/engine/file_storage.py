35cdd93ffbba34c8ebdd34e196cbecefac5b08e4
ayaelas <assalharras@gmail.com>
2024-02-21 15:03:21 +0100

    init

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -54,4 +54,5 @@ class FileStorage:
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
+            if key in self.__objects.keys():
+                del self.__objects[key]
