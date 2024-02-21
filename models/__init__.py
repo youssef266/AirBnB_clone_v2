10148e6bcd96d73b9fbdcf3db2d17013f7de037d
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-21 15:44:50 +0200

    init.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,5 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-    storage.reload()
-else:
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
