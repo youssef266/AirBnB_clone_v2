e421eeb11c26135aad70bd6e4085ce5617033e86
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-21 20:41:36 +0200

    review.py

--- models/user.py
+++ models/user.py
@@ -2,14 +2,13 @@
 """This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address String(128) can't be null
-        password: password for you login String(128) can't be null
+        password: password for you login String(128)
         first_name: first name String(128)
         last_name: last name String(128)
     """
@@ -18,7 +17,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
