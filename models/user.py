f347de16908be24f9ad2ec8f8c768a3edef403e5
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-21 20:40:00 +0200

    review.py

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -17,3 +18,7 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
