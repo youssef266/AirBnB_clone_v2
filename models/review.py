9b0c7ff7f6140929a1511903f8b3082d48376202
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-21 20:34:44 +0200

    review.py

--- models/review.py
+++ models/review.py
@@ -5,7 +5,12 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
+    """This is the class for Review
+    Attributes:
+        place_id: place id String(1024)can't be null
+        user_id: user id String(60) can't be null
+        text: review description
+    """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
