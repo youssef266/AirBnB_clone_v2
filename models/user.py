6937bd42ec52e3ba4732b29f4a5efd13947daadf
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-22 23:07:29 +0200

    fix user class

--- models/user.py
+++ models/user.py
@@ -22,8 +22,8 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
         
-        places = relationship("Place",  backref="user", cascade="all,delete")
-        reviews = relationship("Review",  backref="user", cascade="all,delete")
+       # places = relationship("Place",  backref="user", cascade="all,delete")
+       # reviews = relationship("Review",  backref="user", cascade="all,delete")
     else:
         email = ''
         password = ''
