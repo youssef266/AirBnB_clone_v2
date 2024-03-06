2ceca6043027c6fa9e501f5395206275c9c2dad1
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-06 16:31:43 +0200

    hot fix

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from os import getenv
+import os
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-    storage.reload()
-else:
-    storage = FileStorage()
-    storage.reload()
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A unique FileStorage/DBStorage instance for all models.
+"""
+storage.reload()
